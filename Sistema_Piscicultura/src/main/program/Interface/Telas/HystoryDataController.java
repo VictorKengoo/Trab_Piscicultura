0b436d9a87ddaa8bafb68a30d338ea4277309202
Guilherme Quaquio Xavier <guilherme.q.xavier@accenture.com>
2020-04-10 01:36:52 -0300

    HistoryData tela

---
+++ Sistema_Piscicultura/src/main/program/Interface/Telas/HystoryDataController.java
@@ -0,0 +1,17 @@
+package main.program.Interface.Telas;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+
+public class HystoryDataController {
+
+    @FXML
+    private LineChart <String,Integer> lineChartpH;
+
+    private CategoryAxis categoryAxispH;
+
+    private NumberAxis numberAxispH;
+
+}
