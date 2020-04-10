d60097a0b37b7f78fbbc6b280f856977dd2a8c85
Fábio H. dos S. Rocha <55759993+FabioHSR@users.noreply.github.com>
2020-04-10 01:37:23 -0300

    Application

--- Sistema_Piscicultura/src/main/program/Application/TanqueApp.java
+++ Sistema_Piscicultura/src/main/program/Application/TanqueApp.java
@@ -1,4 +1,11 @@
 package main.program.Application;
 
-public class TanqueApp {
+import main.program.Models.Tanque;
+import main.program.Repository.TanqueRepository;
+
+public class TanqueApp extends BaseApp<Tanque> {
+
+    private TanqueRepository _tanqueRepository;
+
+    public TanqueApp() { _tanqueRepository = new TanqueRepository(); }
 }
